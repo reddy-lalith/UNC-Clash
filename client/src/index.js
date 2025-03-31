126acf50197d71ed3d43527782d2237086a0f547
Lalith Reddy <lalithreddy7@gmail.com>
2025-03-31 15:44:10 -0400

    fix: Correct App component import path case

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/profiles.css';
-import App from './App';  // Note the capital 'A'
+import App from './App';  // Changed to uppercase './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
