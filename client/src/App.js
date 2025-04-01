9518a7ef4aa128bc626f01ea612cfea734c9ddf1
Lalith Reddy <lalithreddy7@gmail.com>
2025-04-01 12:56:41 -0400

    adding tracking to vercel

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from './context/ThemeContext';
+import { Analytics } from "@vercel/analytics/react"
 import DarkModeToggle from './components/DarkModeToggle';
 import Profiles from './pages/Profiles';
 import Leaderboard from './pages/Leaderboard';
@@ -19,6 +20,7 @@ function App() {
           <Route path="/leaderboard" element={<Leaderboard />} />
           <Route path="/battle-history" element={<BattleHistory />} />
         </Routes>
+        <Analytics />
       </Router>
     </ThemeProvider>
   );
